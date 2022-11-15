d8e060056bf99d11f242ee7d33457878b4d2d7d8
peterbartahu <113360396+peterbartahu@users.noreply.github.com>
2022-11-15 12:49:58 +0100

    firstCommit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,180 +4,212 @@ import java.util.Scanner;
 
 public class Main {
 
-    private String[][] level = new String[10][10];
-    private final String playerMark = "@";
-    //    private final String playerMark = "\uD83E\uDDD9";
-//    private final String ZOMBIE = "\uD83E\uDDDF";
-    private final String ZOMBIE = "Z";
-    private final String DISH = "o";
-    //    private final String DISH = "\uD83C\uDF4E";
-    Random random = new Random();
-    private int row = 8;
-    private int column = 1;
-    String message = "Your score now: ";
-    private static int currentScore = 0;
-    private final String BORDER = "'";
-
-    private Scanner scanner = new Scanner(System.in);
-    private final static String EMPTY = " ";
-
-
     public static void main(String[] args) {
         new Main().run();
     }
 
-    /**
-     * This method delete previous maps if you run the program in Comman Line.
-     */
-    private void clear() {
+    private void run() {
+        final String[][] level = new String[10][10];
+        int y = 1;
+        int x = 1;
+
+        boolean isCritical = false;
+
+        int currentScore = 0;
+
+
+        String borderCell = "'";
+        String emptyCell = " ";
+
+        mapInit(level, borderCell, emptyCell);
+
+
+        String dish = "o";
+        Random random = new Random();
+        placeRandom(level, dish, random);
+
+
+        String zombie = "Z";
+        placeRandom(level, zombie, random);
+
+
+        for (; ; ) {
+
+            clearScreen();
+            printActions();
+            draw(level, y, x, "@");
+            System.out.println("Your score now: " + currentScore);
+            if (isCritical) {
+                System.out.println("Oops, it's a ZOMBIE. You lost your points.");
+                isCritical = false;
+            }
+
+            Scanner scanner = new Scanner(System.in);
+            switch (scanner.nextLine().toLowerCase()) {
+                case "w":
+                    move(level, x, y, dish, emptyCell, currentScore, zombie, borderCell, isCritical);
+
+                    if (level[y - 1][x].equals(dish)) {
+                        level[y - 1][x] = emptyCell;
+                        placeRandom(level, dish, random);
+                        ++currentScore;
+                    } else if (level[y - 1][x].equals(zombie)) {
+                        level[y - 1][x] = emptyCell;
+                        isCritical = true;
+                        placeRandom(level, zombie, random);
+                        currentScore = 0;
+                    } else if (level[y - 1][x].equals(emptyCell)) {
+                        y--;
+                    } else if (level[y - 1][x].equals(borderCell)) {
+                        y--;
+
+                    }
+                    if (level[y][x].equals(borderCell)) {
+                        y = level.length - 2;
+
+                    }
+                    break;
+
+                case "a":
+                    if (level[y][x - 1].equals(dish)) {
+                        level[y][x - 1] = emptyCell;
+                        placeRandom(level, dish, random);
+                        ++currentScore;
+                    } else if (level[y][x - 1].equals(zombie)) {
+                        level[y][x - 1] = emptyCell;
+                        isCritical = true;
+                        placeRandom(level, zombie, random);
+                        currentScore = 0;
+                    } else if (level[y][x - 1].equals(emptyCell)) {
+                        x--;
+                    } else if (level[y][x - 1].equals(borderCell)) {
+                        x--;
+                    }
+                    if (level[y][x].equals(borderCell)) {
+                        x = level.length - 2;
+                    }
+                    break;
+
+                case "s":
+                    if (level[y + 1][x].equals(dish)) {
+                        level[y + 1][x] = emptyCell;
+                        placeRandom(level, dish, random);
+                        ++currentScore;
+                    } else if (level[y + 1][x].equals(zombie)) {
+                        level[y + 1][x] = emptyCell;
+                        isCritical = true;
+                        placeRandom(level, zombie, random);
+                        currentScore = 0;
+                    } else if (level[y + 1][x].equals(emptyCell)) {
+                        y++;
+                    } else if (level[y + 1][x].equals(borderCell)) {
+                        y++;
+                    }
+                    if (level[y][x].equals(borderCell)) {
+                        y = 1;
+                    }
+                    break;
+
+                case "d":
+                    if (level[y][x + 1].equals(dish)) {
+                        level[y][x + 1] = emptyCell;
+                        placeRandom(level, dish, random);
+                        ++currentScore;
+                    } else if (level[y][x + 1].equals(zombie)) {
+                        level[y][x + 1] = emptyCell;
+                        isCritical = true;
+                        placeRandom(level, zombie, random);
+                        currentScore = 0;
+                    } else if (level[y][x + 1].equals(emptyCell)) {
+                        x++;
+                    } else if (level[y][x + 1].equals(borderCell)) {
+                        x++;
+                    }
+                    if (level[y][x].equals(borderCell)) {
+                        x = 1;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+
+    private static void printActions() {
+        System.out.println("\nAvailable actions:\npress");
+        System.out.println(
+                "0 - to shutdown\n" +
+                        "1 - to print contacts\n" +
+                        "2 - to add a new contact\n" +
+                        "3 - to update an existing contact \n" +
+                        "4 - to remove an existing contact\n" +
+                        "5 - query if an existing contact exists\n" +
+                        "6 - to print a list of available actions.");
+        System.out.println("Choose your action: ");
+    }
+
+
+    private void move(String[][] level, int x, int y, String dish, String emptyCell, int currentScore, String zombie, String borderCell, boolean isCritical) {
+        Random random = new Random();
+        if (level[y - 1][x].equals(dish)) {
+            level[y - 1][x] = emptyCell;
+            placeRandom(level, dish, random);
+            ++currentScore;
+        } else if (level[y - 1][x].equals(zombie)) {
+            level[y - 1][x] = emptyCell;
+            isCritical = true;
+            placeRandom(level, zombie, random);
+            currentScore = 0;
+        } else if (level[y - 1][x].equals(emptyCell)) {
+            y--;
+        } else if (level[y - 1][x].equals(borderCell)) {
+            y--;
+
+        }
+        if (level[y][x].equals(borderCell)) {
+            y = level.length - 2;
+
+        }
+    }
+
+    private void draw(String[][] level, int y, int x, String playerMark) {
+        for (int i = 0; i < level.length; i++) {
+            for (int j = 0; j < level[i].length; j++) {
+                String position;
+                if (i == y && j == x) {
+                    position = playerMark;
+                } else {
+                    position = level[i][j];
+                }
+                System.out.print(position);
+            }
+            System.out.println();
+        }
+    }
+
+    private void mapInit(String[][] level, String borderCell, String emptyCell) {
+        for (int i = 0; i < level.length; i++) {
+            for (int j = 0; j < level[i].length; j++) {
+                if (i == 0 || j == 0 || i == level.length - 1 || j == level.length - 1) {
+                    level[i][j] = borderCell;
+                } else {
+                    level[i][j] = emptyCell;
+                }
+            }
+        }
+    }
+
+    private void placeRandom(String[][] level, String item, Random random) {
+        level[random.nextInt((level.length - 1) - 1) + 1][random.nextInt((level.length - 1) - 1) + 1] = item;
+    }
+
+    private void clearScreen() {
         try {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * This method draw the map, empty cells and borders.
-     */
-    private void mapInit() {
-        for (int i = 0; i < level.length; i++) {
-            for (int j = 0; j < level[i].length; j++) {
-                if (i == 0 || j == 0 || i == level.length - 1 || j == level.length - 1) {
-                    level[i][j] = BORDER;
-                } else {
-                    level[i][j] = EMPTY;
-                }
-            }
-        }
-        dishOnMap();
-        zombieOnMap();
-    }
-
-
-    /**
-     * Place dish on random place
-     */
-    private void dishOnMap() {
-        level[random.nextInt((level.length - 1) - 1) + 1][random.nextInt((level.length - 1) - 1) + 1] = DISH;
-    }
-
-    /**
-     * Zombie on map
-     */
-    private void zombieOnMap() {
-        level[random.nextInt((level.length - 1) - 1) + 1][random.nextInt((level.length - 1) - 1) + 1] = ZOMBIE;
-    }
-
-    private void draw() {
-        clear();
-        for (int i = 0; i < level.length; i++) {
-            for (int j = 0; j < level[i].length; j++) {
-                if (i == row && j == column) {
-                    System.out.print(playerMark);
-                } else {
-                    System.out.print(level[i][j]);
-                }
-            }
-            System.out.println();
-        }
-        System.out.println(message + currentScore);
-    }
-
-    /**
-     * Player moving method
-     */
-    private void processInput() {
-        System.out.println("Please press te direction");
-        switch (scanner.nextLine().toLowerCase()) {
-            case "w":
-                if (level[row - 1][column].equals(DISH)) {
-                    level[row - 1][column] = EMPTY;
-
-                    dishOnMap();
-//                    zombieOnMap();
-                    ++currentScore;
-                    break;
-                }
-
-                if (level[row - 1][column].equals(EMPTY)) {
-                    row--;
-                    break;
-                }
-                if (level[row - 1][column].equals(BORDER)) {
-                    row = level.length - 2;
-                    break;
-                }
-
-            case "a":
-
-                if (level[row][column - 1].equals(DISH)) {
-                    level[row][column - 1] = EMPTY;
-                    dishOnMap();
-//                    zombieOnMap();
-                    ++currentScore;
-                    break;
-                }
-                if (level[row][column - 1].equals(EMPTY)) {
-                    column--;
-                    break;
-                }
-                if (level[row][column - 1].equals(BORDER)) {
-                    column = level.length-2;
-                    break;
-                }
-
-            case "s":
-
-                if (level[row + 1][column].equals(DISH)) {
-                    level[row + 1][column] = EMPTY;
-
-                    dishOnMap();
-//                    zombieOnMap();
-                    ++currentScore;
-                    break;
-                }
-
-                if (level[row + 1][column].equals(EMPTY)) {
-                    row++;
-                    break;
-                }
-                if (level[row + 1][column].equals(BORDER)) {
-                    row = 1;
-                    break;
-                }
-
-            case "d":
-                if (level[row][column + 1].equals(DISH)) {
-                    level[row][column + 1] = EMPTY;
-
-                    dishOnMap();
-//                    zombieOnMap();
-                    ++currentScore;
-                    break;
-                }
-                if (level[row][column + 1].equals(EMPTY)) {
-                    column++;
-                    break;
-                }
-                if (level[row][column + 1].equals(BORDER)) {
-                    column = 1;
-                    break;
-                }
-        }
-    }
-
-    /**
-     * Program run method.
-     */
-    private void run() {
-        mapInit();
-
-        for (; ; ) {
-
-            draw();
-
-            processInput();
         }
     }
 }
